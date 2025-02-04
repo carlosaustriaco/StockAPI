94b2ed5abe3992a858c521a2314a63e7102df132
Carlos <carlos.aaustriaco@gmail.com>
2025-02-04 10:31:52 -0300

    feat/Get data from Yahoo Finances #2

--- stock_api/stock_indicators/views.py
+++ stock_api/stock_indicators/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from .utils import fetch_data
 
-# Create your views here.
+class IndicatorAPI(View):
+    def get(self, request):
+        symbol = request.GET.get('symbol', '').upper()
+        indicator = request.GET.get('indicator', '').lower()
+
+        if (not symbol) and (not indicator):
+            return JsonResponse({'error': 'Missing "symbol" and "indicator" parameter'}, status=400)
+        
+        only_symbol = symbol and (not indicator)
+
+        try:
+            if only_symbol:
+                stock_data = fetch_data(symbol)
+                last_close = stock_data['Close'].iloc[-1]
+                return JsonResponse({'symbol': symbol, 'close' : f"{last_close:.2f}"})
+            
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
